74ea1b0682bf959067f840c06f2646301a755014
Priti Priya <priti.priya@diggibyte.com>
2024-10-26 14:06:07 +0530

    compute

---
+++ src/Assignment1/driver.py
@@ -0,0 +1,12 @@
+from pyspark_assignment.src.Assignment1.util import *
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Purchase_analysis").getOrCreate()
+result = only_product_iphone13(df1)
+result.show()
+
+result =iphone13_to_iphone14(df1)
+result.show()
+result_df1=product_unique(df1,df2)
+result_df1.show()
+

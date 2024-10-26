a334ef12766c6598c54ce6c97565cbbfdb2f4562
Priti Priya <priti.priya@diggibyte.com>
2024-10-26 13:46:32 +0530

    compute

--- src/Assignment1/driver.py
+++ src/Assignment1/driver.py
@@ -1,47 +1,12 @@
-
+from pyspark_assignment.src.Assignment1.util import *
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
 
-def create_spark_session(app_name: str = "CreditCardMasking") -> SparkSession:
-    """
-    Creates a Spark session.
-    """
-    return SparkSession.builder.appName(app_name).getOrCreate()
+spark = SparkSession.builder.appName("Purchase_analysis").getOrCreate()
+result = only_product_iphone13(df1)
+result.show()
 
-def create_credit_card_df(spark: SparkSession, data: list) -> 'DataFrame':
-    """
-    Creates a DataFrame from the given list of tuples.
-    """
-    return spark.createDataFrame(data, ["card_number"])
+result =iphone13_to_iphone14(df1)
+result.show()
+result_df1=product_unique(df1,df2)
+result_df1.show()
 
-def get_num_partitions(df: 'DataFrame') -> int:
-    """
-    Returns the number of partitions in the DataFrame.
-    """
-    return df.rdd.getNumPartitions()
-
-def repartition_df(df: 'DataFrame', num_partitions: int) -> 'DataFrame':
-    """
-    Repartitions the DataFrame to the specified number of partitions.
-    """
-    return df.repartition(num_partitions)
-
-def coalesce_df(df: 'DataFrame', num_partitions: int) -> 'DataFrame':
-    """
-    Coalesces the DataFrame to the specified number of partitions.
-    """
-    return df.coalesce(num_partitions)
-
-def mask_credit_card(card_number: str) -> str:
-    """
-    Masks all but the last 4 digits of the credit card number.
-    """
-    return '*' * (len(card_number) - 4) + card_number[-4:]
-
-def add_masked_column(df: 'DataFrame') -> 'DataFrame':
-    """
-    Adds a masked credit card column to the DataFrame.
-    """
-    mask_udf = udf(mask_credit_card, StringType())
-    return df.withColumn("masked_card_number", mask_udf(df.card_number))
